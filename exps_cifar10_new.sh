544055f01b0707681cc4f6083bdf5a440f3b21b7
Ameya Joshi <ameya.joshi@nyu.edu>
2021-12-10 10:43:36 -0500

    added scripts and results from max smoothing

---
+++ exps_cifar10_new.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+sbatch ./infer_new.sh 32 4 0.25 max
+sbatch ./infer_new.sh 32 4 0.12 max
+sbatch ./infer_new.sh 32 4 0.5 max
+sbatch ./infer_new.sh 32 4 1.0 max
+sbatch ./infer_new.sh 32 2 0.25 max
+sbatch ./infer_new.sh 32 2 0.12 max
+sbatch ./infer_new.sh 32 2 0.5 max
+sbatch ./infer_new.sh 32 2 1.0 max
+sbatch ./infer_new.sh 32 4 0.25 min
+sbatch ./infer_new.sh 32 4 0.12 min
+sbatch ./infer_new.sh 32 4 0.5 min
+sbatch ./infer_new.sh 32 4 1.0 min
+sbatch ./infer_new.sh 32 2 0.25 min
+sbatch ./infer_new.sh 32 2 0.12 min
+sbatch ./infer_new.sh 32 2 0.5 min
+sbatch ./infer_new.sh 32 2 1.0 min
+sbatch ./infer_new.sh 32 4 0.25 mean
+sbatch ./infer_new.sh 32 4 0.12 mean
+sbatch ./infer_new.sh 32 4 0.5 mean
+sbatch ./infer_new.sh 32 4 1.0 mean
+sbatch ./infer_new.sh 32 2 0.25 mean
+sbatch ./infer_new.sh 32 2 0.12 mean
+sbatch ./infer_new.sh 32 2 0.5 mean
+sbatch ./infer_new.sh 32 2 1.0 mean
+
