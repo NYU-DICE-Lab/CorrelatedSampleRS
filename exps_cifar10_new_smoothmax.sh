8803167cc51adfaeddeea849eea1ddd70f6170f7
Ameya Joshi <ameya.joshi@nyu.edu>
2022-01-07 09:21:25 -0500

    Added experiment slurm scripts for smoothmax instead of maxsmooth

---
+++ exps_cifar10_new_smoothmax.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sbatch ./infer_new_smoothmax.sh 32 4 0.25 max
+sbatch ./infer_new_smoothmax.sh 32 4 0.12 max
+sbatch ./infer_new_smoothmax.sh 32 4 0.5 max
+sbatch ./infer_new_smoothmax.sh 32 4 1.0 max
+sbatch ./infer_new_smoothmax.sh 32 2 0.25 max
+sbatch ./infer_new_smoothmax.sh 32 2 0.12 max
+sbatch ./infer_new_smoothmax.sh 32 2 0.5 max
+sbatch ./infer_new_smoothmax.sh 32 2 1.0 max
