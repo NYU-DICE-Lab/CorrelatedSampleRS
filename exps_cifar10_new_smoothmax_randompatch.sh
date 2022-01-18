663079f0aff8d72dea0d0b825dd5c3f553bfa005
Ameya Joshi <ameya.joshi@nyu.edu>
2022-01-18 17:08:55 -0500

    cifar10 expts with Sota model

--- exps_cifar10_new_smoothmax_randompatch.sh
+++ exps_cifar10_new_smoothmax_randompatch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.25 max 25
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.12 max 25
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.5 max 25
-sbatch ./infer_new_smoothmax_rp.sh 32 1 1.0 max 25
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.25 max 50
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.12 max 50
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.5 max 50
-sbatch ./infer_new_smoothmax_rp.sh 32 1 1.0 max 50
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.25 max 100
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.12 max 100
-sbatch ./infer_new_smoothmax_rp.sh 32 1 0.5 max 100
-sbatch ./infer_new_smoothmax_rp.sh 32 1 1.0 max 100
+#sbatch ./infer_new_smoothmax_rp.sh 32 1 0.25 max 25
+#sbatch ./infer_new_smoothmax_rp.sh 32 1 0.12 max 25
+sbatch ./infer_new_smoothmax_rp.sh 32 1 0.50 max 25
+sbatch ./infer_new_smoothmax_rp.sh 32 1 1.00 max 25
+#sbatch ./infer_new_smoothmax_rp.sh 32 1 0.25 max 50
+#sbatch ./infer_new_smoothmax_rp.sh 32 1 0.12 max 50
+sbatch ./infer_new_smoothmax_rp.sh 32 1 0.50 max 50
+sbatch ./infer_new_smoothmax_rp.sh 32 1 1.00 max 50
+#sbatch ./infer_new_smoothmax_rp.sh 32 1 0.25 max 100
+#sbatch ./infer_new_smoothmax_rp.sh 32 1 0.12 max 100
+sbatch ./infer_new_smoothmax_rp.sh 32 1 0.50 max 100
+sbatch ./infer_new_smoothmax_rp.sh 32 1 1.00 max 100
 
--- infer_new_smoothmax_rp.sh
+++ infer_new_smoothmax_rp.sh
@@ -20,10 +20,10 @@ module purge
 singularity exec --nv \
 	--overlay /scratch/aaj458/singularity_containers/my_pytorch.ext3:ro \
 	/scratch/aaj458/singularity_containers/cuda11.1.1-cudnn8-devel-ubuntu20.04.sif \
-	/bin/bash -c "source /ext3/env.sh; python infer_certify_pretrained_salman_uncorrelated.py cifar10 -dpath /scratch/aaj458/data/Cifar10/ -mp salman_models/pretrained_models/cifar10/PGD_2steps/eps_255/cifar10/resnet110/noise_0.50/checkpoint.pth.tar -mt resnet110 -ni 100 -ps $PATCH_SIZE -pstr $PATCH_STRIDE -sigma $SIGMA --N0 100 --N 10000 --patch -o certify_results_salman_patchsmooth_smoothmax_randompatches_$MAXPATCHES/ --batch 400 -rm $RMODE -ns 36 -rp -np $MAXPATCHES"
+	/bin/bash -c "source /ext3/env.sh; python infer_certify_pretrained_salman_uncorrelated.py cifar10 -dpath /scratch/aaj458/data/Cifar10/ -mp salman_models/pretrained_models/cifar10/finetune_cifar_from_imagenetPGD2steps/PGD_10steps_30epochs_multinoise/2-multitrain/eps_64/cifar10/resnet110/noise_$SIGMA/checkpoint.pth.tar -mt resnet110 -ni 100 -ps $PATCH_SIZE -pstr $PATCH_STRIDE -sigma $SIGMA --N0 100 --N 10000 --patch -o cifar10_SOTA/certify_results_salman_patchsmooth_smoothmax_randompatches_$MAXPATCHES/ --batch 400 -rm $RMODE -ns 36 -rp -np $MAXPATCHES --normalize"
 
 
 #singularity exec --nv \
 #	--overlay /scratch/aaj458/singularity_containers/my_pytorch.ext3:ro \
 #	/scratch/aaj458/singularity_containers/cuda11.1.1-cudnn8-devel-ubuntu20.04.sif \
-#	/bin/bash -c "source /ext3/env.sh; python infer_certify_pretrained_salman.py cifar10 -dpath /scratch/aaj458/data/Cifar10/ -mt resnet110 -mp salman_models/pretrained_models/cifar10/PGD_2steps/eps_255/cifar10/resnet110/noise_0.50/checkpoint.pth.tar -ni 100 -ps $PATCH_SIZE -pstr $PATCH_STRIDE -sigma $SIGMA --N0 100 --N 10000 -o certify_results_salman_nopatch/ --batch 400 "
+#	/bin/bash -c "source /ext3/env.sh; python infer_certify_pretrained_salman.py cifar10 -dpath /scratch/aaj458/data/Cifar10/ -mt resnet110 -mp salman_models/pretrained_models/cifar10/finetune_cifar_from_imagenetPGD2steps/PGD_10steps_30epochs_multinoise/2-multitrain/eps_64/cifar10/resnet110/noise_0.12/checkpoint.pth.tar -ni 100 -ps $PATCH_SIZE -pstr $PATCH_STRIDE -sigma $SIGMA --N0 100 --N 10000 -o cifar10_SOTA/ --batch 400 --normalize"
