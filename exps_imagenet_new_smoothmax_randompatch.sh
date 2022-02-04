a59080bf5a4f7f1a50e822c7bd21b560b476ace2
ameya005 <ameya.joshi@nyu.edu>
2022-02-04 10:13:11 -0500

    Added imagenet experiment fles

---
+++ exps_imagenet_new_smoothmax_randompatch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.25 max 10 0  
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.12 max 10 0 
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.50 max 10 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 1.00 max 10 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.25 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.12 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.50 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 1.00 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.25 max 50 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.12 max 50 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 0.50 max 50 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 32 1 1.00 max 50 0
+
