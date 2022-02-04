ada48b056ad3ee09d9a023cd3655d830fb9ae164
ameya005 <ameya.joshi@nyu.edu>
2022-02-04 10:25:13 -0500

    Added new imagenet ddn experiemtn files

---
+++ exps_imagenet_new_smoothmax_randompatch_ddn.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.25 max 10 0  
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.12 max 10 0 
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.50 max 10 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 1.00 max 10 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.25 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.12 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.50 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 1.00 max 25 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.25 max 50 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.12 max 50 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.50 max 50 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 1.00 max 50 0
+
