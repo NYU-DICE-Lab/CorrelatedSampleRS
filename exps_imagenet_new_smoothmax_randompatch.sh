cc049653f98d1af7591e11ba2cbeb6275cebd012
ameya005 <ameya.joshi@nyu.edu>
2022-02-11 19:27:32 -0500

    fixed bug

--- exps_imagenet_new_smoothmax_randompatch.sh
+++ exps_imagenet_new_smoothmax_randompatch.sh
@@ -9,8 +9,13 @@
 # sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 0.25 max 16 0
 # sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 0.50 max 16 0
 # sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 1.00 max 16 0
-sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 0.25 mean 32 0
-sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 0.50 mean 32 0
-sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 1.00 mean 32 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 0.25 mean 16 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 0.50 mean 16 0
+sbatch ./infer_new_smoothmax_rp_imagenet.sh 224 1 1.00 mean 16 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.25 mean 16 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 0.50 mean 16 0
+sbatch ./infer_new_smoothmax_rp_imagenet_ddn.sh 224 1 1.00 mean 16 0
+
+
 
 
